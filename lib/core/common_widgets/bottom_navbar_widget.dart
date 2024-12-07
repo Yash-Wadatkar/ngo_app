af6c87f595b37a5b90354e0bbecd89ef5da9d688
Yash-Wadatkar <yashwadatkar3@gmail.com>
2024-12-08 00:50:43 +0530

    navbar screen added

---
+++ lib/core/common_widgets/bottom_navbar_widget.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class BottomNavbarWidget extends StatelessWidget {
+  const BottomNavbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
